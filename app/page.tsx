10d84f9891e9ea2ff081357b4bb2eb2cfc28b1c9
ashleytrice2006 <ashley@smartroads.co.nz>
2025-02-19 16:42:04 +1300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image 
-  src="/your-logo-01.png"  // Make sure this matches the exact file name in GitHub
+  src="/public/your-logo-01.png" 
   alt="Smart Roads Logo" 
   width={250} 
   height={150} 
