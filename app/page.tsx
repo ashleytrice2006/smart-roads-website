12508b12480f609ab7288bf294503258d82a231d
ashleytrice2006 <ashley@smartroads.co.nz>
2025-02-19 17:04:27 +1300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,6 @@ export default function Home() {
   width={250} // Adjust as needed
   height={100}
 />
-    </div>
-  );
-/>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
