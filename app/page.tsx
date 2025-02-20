ee41cc5c458cb1d1e047b63b8c8d345267933795
ashleytrice2006 <ashley@smartroads.co.nz>
2025-02-20 15:20:33 +1300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<Image
-   src="/logo.png" 
-   alt="Smart Roads Logo" 
-   width={250} 
-   height={100} 
-   priority 
-   unoptimized
+<img
+  src="/logo.png"
+  alt="Smart Roads Logo"
+  width="250"
+  height="100"
 />
     </div>
   );
